f5809a6659c3cdaf5a6e297e62efde7dc79ab24c
Thanh Nguyen <thanhnt94@gmail.com>
2026-08-02 18:53:19 +0700

    Auto commit from remote update tool - 2026-08-02 18:53:19

--- app/modules/deck/routes/play.py
+++ app/modules/deck/routes/play.py
@@ -2669,7 +2669,7 @@ async def get_deck_roadmap_status_helper(db: AsyncSession, user_id: int, deck_id
             })
         elif stype == "fsrs_review":
             overdue_hours = int(st.get("overdue_hours", 24))
-            is_done = (review_due_today == 0) or (review_completed_today >= review_due_today)
+            is_done = (review_due_today <= 0)
             step_data.update({
                 "overdue_hours": overdue_hours,
                 "done": is_done,
