6bdddaaf492bdc3098cef536066fdb4e75a96541
Thanh Nguyen <thanhnt94@gmail.com>
2026-08-13 08:36:38 +0700

    import User in fix script

--- fix_aug12_invalid_progress.py
+++ fix_aug12_invalid_progress.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import select
 from app.core.config import settings
+from app.modules.auth.models import User
 from app.modules.deck.models import UserDailyProgress, UserDeckGoal
 
 async def fix_progress():
